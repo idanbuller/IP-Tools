b19d969d12b27d2db7f3fcd8631d7ea40fb6d293
IB <49998731+idanbuller@users.noreply.github.com>
2020-08-02 00:20:36 +0300

    Delete Site_IP.py

--- Site_IP.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-user_input = str(input("Example: hacker.io \nEnter the domain name: "))
-
-hostname = user_input
-
-hostname_ip = socket.gethostbyname(hostname)
-
-print(hostname_ip)
